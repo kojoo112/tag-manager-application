821bc012f68116b50e9ab100ea3671e813fc7889
kojoo112 <kapella000@gmail.com>
2022-05-20 16:40:56 +0900

    cameraView 구현

--- src/components/StorageItemList.tsx
+++ src/components/StorageItemList.tsx
@@ -5,15 +5,14 @@ interface IReference {
   bucket: string;
   fullPath: string;
   name: string;
-  parent: string;
-  root: string;
-  storage: string;
+  parent: IReference;
+  root: IReference;
+  // storage: string;
 }
 const prefix =
   "https://firebasestorage.googleapis.com/v0/b/xcape-hint-app.appspot.com/o/images%2F";
 const suffix = "?alt=media";
 
-const imageURL = `${prefix}`;
 const StorageItemList = () => {
   const [storageItems, setStorageItems] = useState<any>();
 
@@ -25,7 +24,16 @@ const StorageItemList = () => {
   }, []);
 
   return (
-    <div style={{ color: "white" }}>
+    <div
+      style={{
+        color: "white",
+        overflowY: "scroll",
+        height: "30%",
+        backgroundColor: "black",
+        marginTop: 10,
+        paddingTop: 10,
+      }}
+    >
       {storageItems &&
         storageItems.map((element: any, index: number) => {
           return (
